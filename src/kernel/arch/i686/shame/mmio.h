386b6d1221773fd1e685f118f36a323d24c13c09
Evan Liu <evanyxliu@gmail.com>
2025-12-04 16:51:04 -0500

    cleaned up the shame folder it was getting in the way of compilation

--- src/kernel/arch/i686/shame/mmio.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <stdint.h>
-#include <stddef.h>
-
-uint32_t i686_MMIO_Read32(volatile void* addr);
-void i686_MMIO_Write32(volatile void* addr, uint32_t value);
-void* mmio_map(uintptr_t phys_addr, size_t size);
