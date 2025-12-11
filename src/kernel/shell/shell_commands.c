c8567465a12157e36453585e351b9728d9fd19b6
Evan Liu <evanyxliu@gmail.com>
2025-12-11 00:06:09 -0500

    mixxed files from last commit

--- src/kernel/shell/shell_commands.c
+++ src/kernel/shell/shell_commands.c
@@ -2,7 +2,8 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdbool.h>
-#include "kstring/string.h"
+#include <kstring/string.h>
+#include <kfat/fat.h>
 
 typedef struct {
     const char* name;
@@ -12,8 +13,14 @@ typedef struct {
 const Command shell_command_list[] = {
     { "help", "help" },
     { "froggo", "froggo" },
-    { "echo", "echo [-n] [string...] -o [> | >>] [file name]" }, 
-    { "clear", "clear" }
+    { "clear", "clear" },
+    { "ls", "ls" },
+    { "mkdir", "mkdir [dir name]" },
+    { "mkfile", "mkfile [file name]" },
+    { "rmdir", "rmdir [dir name]" },
+    { "rmfile", "rmfile [file name]" },
+    { "echo", "echo -o [-n] [string...] -o [> | >>] [file name]" }, 
+    { "cat", "cat [file name...]"}
 };
 
 const int command_count = sizeof(shell_command_list) / sizeof(Command);
@@ -28,13 +35,17 @@ void Shell_Help() {
         printf("%s\n usage: %s\n", shell_command_list[i].name, shell_command_list[i].usage);
 }
 
-void Shell_Echo(ParsedCommand* pc) {
-    bool trail = true;
-    int arg = 1;
+void Shell_LS(Directory* dir) {
+    FS_LS(dir);
+}
 
-    if (pc->argc > 1 && strncmp(pc->argv[1], "-n", 3) == 0) {
-        trail = false;
-        arg = 2;
+void Shell_Echo(ParsedCommand* pc, Directory* dir) {
+    bool trail = pc->mode == NO_REDIRECT;
+    int arg = 0;
+
+    if (pc->argc > 1 && strncmp(pc->argv[0], "-n", 3) == 0) {
+        trail = !trail;
+        arg = 1;
     }
     
     KString ks = KS_Init();
@@ -43,11 +54,64 @@ void Shell_Echo(ParsedCommand* pc) {
         KS_Append(&ks, pc->argv[i]);
     }
     if (trail) KS_AppendChar(&ks, '\n');
-
-    if (pc->mode == NO_REDIRECT) {
+    
+    if (pc->mode == REDIRECT_APPEND) {
+        FS_FileAppend(dir, pc->out_file, ks.string, ks.len);
+    } else if (pc->mode == REDIRECT_TRUNC) {
+        FS_FileWrite(dir, pc->out_file, ks.string, ks.len);
+    } else {
         printf("%s", ks.string);
         return;
     }
-
     KS_Free(&ks);
+}
+
+void Shell_Cat(ParsedCommand* pc, Directory* dir) {
+    if (pc->argc == 0) {
+        printf("usage: cat [file name]\n");
+        return;
+    }
+    for (int i = 0; i < pc->argc; i++) {
+        FS_FilePrint(dir, pc->argv[i]);
+    }
+}
+
+void Shell_CD(ParsedCommand* pc, Directory* dir) {
+    if (pc->argc == 0) {
+        printf("usage: cd [dir name]\n");
+        return;
+    }
+    FS_CD(dir, pc->argv[0]);
+}
+
+void Shell_Mkdir(ParsedCommand* pc, Directory* dir) {
+    if (pc->argc == 0) {
+        printf("usage: mkdir [dir name]\n");
+        return;
+    }
+    FS_DirCreate(dir, pc->argv[0]);
+}
+
+void Shell_Mkfile(ParsedCommand* pc, Directory* dir) {
+    if (pc->argc == 0) {
+        printf("usage: mkfile [file name]\n");
+        return;
+    }
+    FS_FileCreate(dir, pc->argv[0]);
+}
+
+void Shell_Rmdir(ParsedCommand* pc, Directory* dir) {
+    if (pc->argc == 0) {
+        printf("usage: mkdir [dir name]\n");
+        return;
+    }
+    FS_DirDelete(dir, pc->argv[0]);
+}
+
+void Shell_Rmfile(ParsedCommand* pc, Directory* dir) {
+    if (pc->argc == 0) {
+        printf("usage: mkfile [file name]\n");
+        return;
+    }
+    FS_FileDelete(dir, pc->argv[0]);
 }
--- src/kernel/shell/shell_commands.h
+++ src/kernel/shell/shell_commands.h
@@ -1,6 +1,18 @@
 #pragma once
 #include "shell_parse.h"
+#include <kfat/fat.h>
 
 void Shell_Froggo(ParsedCommand* pc);
 void Shell_Help();
-void Shell_Echo(ParsedCommand* pc);
+
+void Shell_Echo(ParsedCommand* pc, Directory* dir);
+void Shell_Cat(ParsedCommand* pc, Directory* dir);
+
+void Shell_LS(Directory* dir);
+void Shell_CD(ParsedCommand* pc, Directory* dir);
+
+void Shell_Mkdir(ParsedCommand* pc, Directory* dir);
+void Shell_Mkfile(ParsedCommand* pc, Directory* dir);
+void Shell_Rmdir(ParsedCommand* pc, Directory* dir);
+void Shell_Rmfile(ParsedCommand* pc, Directory* dir);
+
